24227d76aacf7c7cdb024bd3c12ffc3a1f6f4254
kaziarman23 <kaziarman@proton.me>
2024-05-16 08:06:38 +0600

    component part is complite

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -1,4 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "postcss/lib/container";
+import Logo from "../Container/Logo";
+import LogoutBtn from "../Header/LogoutBtn";
 
-export {Header, Footer}
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
